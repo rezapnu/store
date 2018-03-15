2a44e7f5d83f94e72973984c8982dfd1ca163150
rezapnu <rezamofidi66@gmail.com>
2018-03-15 16:56:17 +0000

    concat file js in datatable.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-var hi = require('gulp');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-hi.task('adminlab', function () {
+gulp.task('adminlab', function () {
     console.log('Hello world');
 });
 
-hi.task('one', function () {
+gulp.task('one', function () {
     console.log('task one');
 });
 
-hi.task('two', ['adminlab' , 'one'] , function () {
+gulp.task('two', ['adminlab' , 'one'] , function () {
     console.log('task two');
+});
+
+gulp.task('concat_js', function () {
+    gulp
+        .src([
+            './assets/bootstrap/js/bootstrap.min.js',
+            './js/jquery.blockui.js',
+            './assets/uniform/jquery.uniform.min.js',
+            './assets/data-tables/jquery.dataTables.js',
+            './assets/data-tables/DT_bootstrap.js',
+            './js/scripts.js'
+        ])
+        .pipe(concat("app.js"))
+        .pipe(gulp.dest("./dest"));
 });
