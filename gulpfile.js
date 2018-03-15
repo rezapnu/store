beca0f1e669e714eb9ad97178ee688c4ec279ab3
rezapnu <rezamofidi66@gmail.com>
2018-03-15 15:57:48 +0000

    task

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,13 @@ var hi = require('gulp');
 hi.task('adminlab', function () {
     console.log('Hellocfhgfh world');
 });
+
+var hi = require('gulp');
+hi.task('adminlab', function () {
+    console.log('Hellocfhgfh world');
+});
+
+var hi = require('gulp');
+hi.task('adminlab', function () {
+    console.log('Hellocfhgfh world');
+});
